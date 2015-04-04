ed8475a6fcced190f2e73573b28f2a8ec2b9b1e7
Dmitry Stefantsov <d.a.stefantsov@gmail.com>
2015-04-04 23:23:15 +0600

    Refactor basic view representation
    * prepare for animations of different types
    * prepare for moving screen

--- static/js/animation.js
+++ static/js/animation.js
@@ -1,13 +1,23 @@
-var fadeInAll = function ($el) {
-    var promise = $el.find('.faded1').fadeIn('slow').promise();
+var animateIn = function($el) {
+    var $elIn = $el.find('.in');
     var continuations = new Array();
     [1, 2, 3, 4, 5, 6, 7, 8, 9].forEach(function (i) {
         continuations.push(function () {
-            console.log('working with .faded'+i);
-            return $el.find('.faded'+i).fadeIn('slow').promise();
+            var $elInOrd = $elIn.filter('.ord'+i);
+            var deferred = new $.Deferred();
+            var count = 1;
+            var resolveOnCount = function () {
+                --count;
+                if (count == 0) {
+                    deferred.resolve();
+                }
+            };
+            $elInOrd.filter('.fade').animate(
+                {opacity: '1.0'},
+                'slow'
+            ).promise().then(resolveOnCount);
+            return deferred.promise();
         });
     });
-    promisePipe(promise, continuations).then(function () {
-        console.log('done with animations');
-    });
+    promisePipe(promiseUnit(), continuations);
 }
