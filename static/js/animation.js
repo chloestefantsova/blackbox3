f52fcd9d65d920af466ed530b38a6fe4994fa724
Dmitry Stefantsov <d.a.stefantsov@gmail.com>
2015-04-05 01:28:33 +0600

    Introduce screen moving functionality

--- static/js/animation.js
+++ static/js/animation.js
@@ -1,4 +1,9 @@
-var SlrAnimation = function () {
+var SlrAnimation = function (homeName) {
+    this.left = 0;
+    this.top = 0;
+    this.geo = {};
+    this.name = homeName;
+    this.putGeo(homeName, 0, 0);
 };
 
 SlrAnimation.prototype = {
@@ -14,7 +19,7 @@ SlrAnimation.prototype = {
         }
     },
 
-    animate: function ($rootEl, type) {
+    animateInOut: function ($rootEl, type) {
         var self = this;
         var continuations = new Array();
         [1, 2, 3, 4, 5, 6, 7, 8, 9].forEach(function (i) {
@@ -31,15 +36,40 @@ SlrAnimation.prototype = {
                 return deferred.promise();
             });
         });
-        promisePipe(promiseUnit(), continuations);
+        return promisePipe(promiseUnit(), continuations);
     },
 
     animateIn: function ($rootEl) {
-        return this.animate($rootEl, 'in');
+        return this.animateInOut($rootEl, 'in');
     },
 
     animateOut: function ($rootEl) {
-        return this.animate($rootEl, 'out');
+        return this.animateInOut($rootEl, 'out');
+    },
+
+
+    putGeo: function (name, screensX, screensY) {
+        this.geo[name] = [screensX, screensY];
+    },
+
+    moveTo: function (name) {
+        var self = this;
+        if (this.geo.hasOwnProperty(name)) {
+            var screensX = this.geo[name][0];
+            var screensY = this.geo[name][1];
+            var destX = screensX * $(window).width();
+            var destY = screensY * $(window).height();
+            return promisePipe(this.animateOut($(this.name)), [
+                    function () {
+                        return $('.screenful').animate({left: destX, top: destY}, 'slow').promise();
+                    },
+                    function () {
+                        self.name = name;
+                        return self.animateIn($(self.name));
+                    }
+            ]);
+        }
+        return promiseUnit();
     },
 
 };
