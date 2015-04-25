6de1eb3e3279af6fa09ddafd0e220becac7edf21
Dmitry Stefantsov <d.a.stefantsov@gmail.com>
2015-04-25 23:45:13 +0600

    Add permission checks in author app

--- author/urls.py
+++ author/urls.py
@@ -2,10 +2,26 @@ from django.conf.urls import patterns
 from django.conf.urls import url
 from django.views.generic.base import RedirectView
 from django.core.urlresolvers import reverse_lazy
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.auth.models import Permission
 from django.contrib.auth.views import login
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import permission_required
 
 from author import views
+
+
+def author_required(function=None, login_url=None):
+    author_permission = Permission(
+        content_type=ContentType.objects.get(app_label='game',
+                                             model='task'),
+        codename='add_task',
+    )
+    actual_decorator = permission_required(author_permission,
+                                           login_url=login_url)
+    if function is None:
+        return actual_decorator(login_required)
+    return actual_decorator(login_required(function))
 
 
 urlpatterns = patterns(
@@ -20,7 +36,7 @@ urlpatterns = patterns(
         name='author-login'),
 
     url(r'^panel/$',
-        login_required(function=views.PanelView.as_view(),
-                       login_url=reverse_lazy('author-login')),
+        author_required(function=views.PanelView.as_view(),
+                        login_url=reverse_lazy('author-login')),
         name='panel'),
 )
